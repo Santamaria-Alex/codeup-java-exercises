00a95643969b94d07ce8d8a36d305bc873b45d25
Alex Santamaria <santamariaalex146@gmail.com>
2021-06-04 08:35:45 -0500

    completed server name generator

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -2,9 +2,9 @@ public class ServerNameGenerator {
 
 
     //Create two arrays whose elements are strings: one with at least 10 adjectives, another with at least 10 nouns.
-    String[] adj = {"adorable", "adventurous", "aggressive", "agreeable", "alert", "beautiful", "charming", "disturbed", "elegant", "frantic", };
+    private static String [] adj = {"adorable", "adventurous", "aggressive", "agreeable", "alert", "beautiful", "charming", "disturbed", "elegant", "frantic", };
 
-    String[] noun = {"time", "year", "people", "way", "day", "man", "thing", "woman", "life", "child", };
+    private static String[] noun = {"time", "year", "people", "way", "day", "man", "thing", "woman", "life", "child", };
 
     //Create a method that will return a random element from an array of strings.
     private static String random(String[] strings){
@@ -13,7 +13,16 @@ public class ServerNameGenerator {
     }
 
 //    public static String makeServerName (String[])
+    public static String makeServerName (String[] arr1, String[] arr2){
+        String adj = random(arr1);
+        String noun = random(arr2);
+        return adj + "-" + noun;
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Here is your server name: ");
+        System.out.println(makeServerName(adj, noun));
+    }
 
 
 
