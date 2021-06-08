477c35988dc7fa129e71c6b322d45762e9646a59
Alex Santamaria <santamariaalex146@gmail.com>
2021-06-08 12:02:34 -0500

    completed student grades app

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,19 +1,59 @@
 package grades;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradesApplication {
 
     public static void main(String[] args) {
 
-        HashMap<String, String> students = new HashMap<>();
-        students.put("Alex", "Cryptic_Shogun");
-        students.put("Alexis", "Shogunator");
-        students.put("Santamaria", "The Nina, the Pinta");
-        students.put("Hasbulla", "GOAT");
+        HashMap<String, Student> students = new HashMap<>();
+
+        students.put("Cryptic_Shogun", new Student("Alex"));
+        students.get("Cryptic_Shogun").addGrade(100);
+        students.get("Cryptic_Shogun").addGrade(80);
+
+        students.put("Shogunator", new Student("Alexis"));
+        students.get("Shogunator").addGrade(80);
+        students.get("Shogunator").addGrade(70);
+
+        students.put("The Nina the Pinta", new Student("Santamaria"));
+        students.get("The Nina the Pinta").addGrade(70);
+        students.get("The Nina the Pinta").addGrade(60);
+
+        students.put("GOAT", new Student("GOAT"));
+        students.get("GOAT").addGrade(50);
+        students.get("GOAT").addGrade(40);
+
+        System.out.println("Usernames:");
+        System.out.println(students.keySet());
+
+        boolean confirm = true;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Select username to view information:");
+            String userInput = scanner.nextLine();
+            if (!students.containsKey(userInput)){
+                System.out.println("User does not exist, re-enter username:");
+            } else if (students.containsKey(userInput)){
+                Student userInputResult = students.get(userInput);
+                System.out.println("Username: " + userInput + "\nStudent: " + userInputResult.getName() + "\nGrade Average: " + userInputResult.getGradeAverage());
+
+                System.out.println("Do you want to see another student's information? [Y/N]");
+                String userInput2 = scanner.nextLine();
+                if (userInput2.equalsIgnoreCase("y")){
+                    System.out.println("Users: ");
+                    System.out.println(students.keySet());
+                }
+                if (!userInput2.equalsIgnoreCase("y")){
+                    System.out.println("See ya.");
+                    break;
+                }
+            }
+        } while (confirm);
 
 
-        System.out.println(students);
 
     }
 
